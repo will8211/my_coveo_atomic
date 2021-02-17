73252e8eef2f9e315cddf7f5c29bdbab841f091a
William Mannard <wmannard@coveo.com>
2021-02-17 10:57:48 -0500

    updated to use latest alpha and test page

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,28 @@ const gulp = require ('gulp')
 const debug = require('gulp-debug');
 const exec = require('child_process').exec;
 const livereload = require('gulp-livereload');
+const download = require("gulp-download");
 
-gulp.task('copyTestPages', function () {
-    return gulp.src(['./testPages/*.html'], { "base" : "./testPages" })
-        .pipe(gulp.dest('./public/testPages'))
+gulp.task('installAtomicAlpha', function (cb) {
+    exec('npm i @coveo/atomic@alpha', function (err, stdout, stderr) {
+        console.log(stdout);
+        console.log(stderr);
+        cb(err);
+    });
+});
+
+gulp.task('getTestPage', function () {
+    return download('https://raw.githubusercontent.com/coveo/ui-kit/master/packages/atomic/src/index.html')
+	.pipe(gulp.dest("public/"));
 });
 
 gulp.task("copyResource", function () {
-    return gulp.src([
-            './node_modules/@coveo/atomic/dist/atomic/**/*'],
-        { "base" : "./node_modules/@coveo/atomic/dist/" })
-        .pipe(gulp.dest('./public/'))
+    return gulp.src(['./node_modules/@coveo/atomic/dist/atomic/**/*'])
+        .pipe(gulp.dest('./public/build/'))
         .pipe(debug());
 
 })
+
 gulp.task('serveStart', function (cb) {
     exec('serve', function (err, stdout, stderr) {
         console.log(stdout);
@@ -23,11 +31,12 @@ gulp.task('serveStart', function (cb) {
         cb(err);
     });
 });
+
 gulp.task('watch', function() {
     livereload.listen();
     gulp.watch('./testPages/*.html', gulp.series('copyTestPages'));
 
 });
-gulp.task('default', gulp.series('copyResource','copyTestPages',function () {}));
 
-gulp.task('dev', gulp.series('copyResource','copyTestPages', gulp.parallel('serveStart', 'watch'),function () {}));
+gulp.task('default', gulp.series('installAtomicAlpha', 'copyResource','getTestPage',function () {}));
+gulp.task('dev', gulp.series('installAtomicAlpha', 'copyResource','getTestPage', gulp.parallel('serveStart', 'watch'),function () {}));
--- public/atomic/index.esm.js
+++
@@ -1,3 +0,0 @@
-import './utils-7d7e1c0f.js';
-import './headless.esm-6bb95796.js';
-export { b as bindLogDocumentOpenOnResult } from './result-utils-4df999ac.js';
