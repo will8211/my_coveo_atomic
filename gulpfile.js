2832dc2b0289c70b462b2032eacae87988f41069
William Mannard <wmannard@coveo.com>
2021-02-17 14:03:36 -0500

    gulp refactor

--- gulpfile.js
+++ gulpfile.js
@@ -4,39 +4,38 @@ const exec = require('child_process').exec;
 const livereload = require('gulp-livereload');
 const download = require("gulp-download");
 
-gulp.task('installAtomicAlpha', function (cb) {
+function installAtomicAlpha(cb) {
     exec('npm i @coveo/atomic@alpha', function (err, stdout, stderr) {
         console.log(stdout);
         console.log(stderr);
         cb(err);
     });
-});
+}
 
-gulp.task('getTestPage', function () {
+function getTestPage() {
     return download('https://raw.githubusercontent.com/coveo/ui-kit/master/packages/atomic/src/index.html')
 	.pipe(gulp.dest('public/'));
-});
+}
 
-gulp.task("copyResource", function () {
+function copyResource() {
     return gulp.src(['./node_modules/@coveo/atomic/dist/atomic/**/*'])
         .pipe(gulp.dest('./public/build/'))
         .pipe(debug());
+}
 
-})
-
-gulp.task('serveStart', function (cb) {
+function serveStart(cb) {
     exec('serve', function (err, stdout, stderr) {
         console.log(stdout);
         console.log(stderr);
         cb(err);
     });
-});
+}
 
-gulp.task('watch', function() {
+function watch() {
     livereload.listen();
     gulp.watch('./testPages/*.html', gulp.series('copyTestPages'));
 
-});
+}
 
-gulp.task('default', gulp.series('installAtomicAlpha', 'copyResource','getTestPage',function () {}));
-gulp.task('dev', gulp.series('installAtomicAlpha', 'copyResource','getTestPage', gulp.parallel('serveStart', 'watch'),function () {}));
+exports.default = gulp.series(installAtomicAlpha, copyResource, getTestPage);
+exports.dev = gulp.series(installAtomicAlpha, copyResource, getTestPage, gulp.parallel(serveStart, watch));
